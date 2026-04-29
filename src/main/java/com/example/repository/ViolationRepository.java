b739b0301fbb23d59efe7704b5d11ff58bf00df6
memaitri <itsmemaitri13@gmail.com>
2026-04-29 12:22:54 +0530

    cleanup: remove old com.example package

--- src/main/java/com/example/repository/ViolationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-// import com.example.model.Violation;
-import com.example.model.Violation;
-
-public interface ViolationRepository extends JpaRepository<Violation, Integer> {
-}
